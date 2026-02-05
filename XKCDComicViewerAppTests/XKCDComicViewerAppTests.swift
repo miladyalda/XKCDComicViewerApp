ad63eecb22997028cf71cb38cbb23d9225c911ee
milad <milad_gbg@hotmail.com>
2026-02-05 01:17:07 +0100

    Initial Commit

---
+++ XKCDComicViewerAppTests/XKCDComicViewerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  XKCDComicViewerAppTests.swift
+//  XKCDComicViewerAppTests
+//
+//  Created by milad yalda on 2026-02-05.
+//
+
+import Testing
+@testable import XKCDComicViewerApp
+
+struct XKCDComicViewerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
